937f0773a48b9d8f27054ea6bec6dea732c6f9f4
anchorz <andreas.ziermann@mrboot.de>
2017-11-02 10:55:30 +0100

    Icon wird jetzt auch beim Mac im Dock angezeigt

---
+++ src/jkcemu/MacOs.java
@@ -0,0 +1,34 @@
+package jkcemu;
+
+import java.awt.Image;
+import java.awt.Window;
+import java.net.URL;
+
+class MacOs {
+
+    public static boolean exists(String className)
+    {
+        try {
+            Class.forName( className, false, null );
+            return true;
+        }
+        catch (ClassNotFoundException exception) {
+            return false;
+        }
+    }
+
+    public static void setIconImages(Window window, String iconImage) {
+        URL url = window.getClass().getResource( iconImage);
+        if( url == null ) {
+            return;
+        }
+        Image img = window.getToolkit().createImage( url );
+        if( img == null ) {
+            return;
+        }
+        if ( exists( "com.apple.eawt.Application" ) )
+        {
+           com.apple.eawt.Application.getApplication().setDockIconImage( img );
+        }
+    }
+}
